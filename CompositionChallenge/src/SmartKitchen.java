fa3d633fa2e23853076ef8774574c7363cba95db
jaesungryu <cwryu6252@naver.com>
2023-10-18 07:39:31 +0900

    composition

---
+++ CompositionChallenge/src/SmartKitchen.java
@@ -0,0 +1,56 @@
+public class SmartKitchen {
+    private CoffeeMaker brewMaster;
+    private Refrigerator iceBox;
+    private DishWasher dishWasher;
+
+    public SmartKitchen() {
+        brewMaster = new CoffeeMaker();
+        iceBox = new Refrigerator();
+        dishWasher = new DishWasher();
+    }
+
+    public CoffeeMaker getBrewMaster() {
+        return brewMaster;
+    }
+
+    public Refrigerator getIceBox() {
+        return iceBox;
+    }
+
+    public DishWasher getDishWasher() {
+        return dishWasher;
+    }
+
+    public void setKitchenState(boolean coffeeFlag, boolean fridgeFlag,
+                                boolean dishWasherFlag) {
+
+        brewMaster.setHasWorkToDo(coffeeFlag);
+        iceBox.setHasWorkToDo(fridgeFlag);
+        dishWasher.setHasWorkToDo(dishWasherFlag);
+    }
+
+    public void doKitchenWork() {
+        brewMaster.brewCoffee();
+        iceBox.orderFood();
+        dishWasher.doDishes();
+    }
+
+
+    public void addWater() {
+        brewMaster.brewCoffee();
+    }
+
+    public void pourMilk() {
+        iceBox.orderFood();
+    }
+
+    public void loadDishWasher() {
+        dishWasher.doDishes();
+    }
+
+    public void setKitchenState() {
+        addWater();
+        pourMilk();
+        loadDishWasher();
+    }
+}
