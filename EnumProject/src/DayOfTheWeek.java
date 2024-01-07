6ef436006443d4012e0ffdb78f20084617fe43d3
jaesungryu <cwryu6252@naver.com>
2024-01-07 16:07:44 +0900

    Enum

---
+++ EnumProject/src/DayOfTheWeek.java
@@ -0,0 +1,3 @@
+public enum DayOfTheWeek {
+    SUN, MON, TUES, WED, THU, FRI, SAT
+}
---
+++ EnumProject/src/Topping.java
@@ -0,0 +1,15 @@
+public enum Topping {
+    MUSTARD,
+    PICKLES,
+    BACON,
+    CHEDDAR,
+    TOMATO;
+
+    public double getPrice() {
+        return switch (this) {
+            case BACON -> 1.5;
+            case CHEDDAR -> 0.5;
+            default -> 0.0;
+        };
+    }
+}
